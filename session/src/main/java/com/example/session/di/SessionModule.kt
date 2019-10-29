e6678e2be934dfa13acaca670499d6b50a80d85c
Etienne Cuvillon <etienne.cuvillon@thescore.com>
2019-10-29 13:58:23 -0400

    Update session scope name

--- session/src/main/java/com/example/session/di/SessionModule.kt
+++ session/src/main/java/com/example/session/di/SessionModule.kt
@@ -1,8 +1,9 @@
 package com.example.session.di
 
 import com.example.session.LoggedSessionManager
-import com.example.session.SessionManager
 import com.example.session.UnloggedSessionManager
+import com.example.session.ext.SESSION_SCOPE_NAME
+import com.example.session.ext.getSessionScope
 import com.example.session.model.Session
 import com.example.session.util.FakeSessionGenerator
 import org.koin.core.qualifier.named
@@ -11,9 +12,9 @@ import org.koin.dsl.module
 val sessionModule = module {
     single { FakeSessionGenerator() }
 
-    scope(named<SessionManager>()) {
+    scope(named(SESSION_SCOPE_NAME)) {
         scoped {
-            val session = getOrNull<Session>()
+            val session = getSessionScope().getOrNull<Session>()
             if (session == null) {
                 UnloggedSessionManager(fakeSessionGenerator = get())
             } else {
--- session/src/main/java/com/example/session/ext/KoinExt.kt
+++ session/src/main/java/com/example/session/ext/KoinExt.kt
@@ -1,12 +1,12 @@
 package com.example.session.ext
 
-import com.example.session.SessionManager
 import com.example.session.model.Session
 import org.koin.core.Koin
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 
-private const val SESSION_SCOPE_ID = "SESSION"
+private const val SESSION_SCOPE_ID = "SESSION_ID"
+internal const val SESSION_SCOPE_NAME = "SESSION_SCOPE_NAME"
 
 /**
  * Extension function so modules don't have to handle the session scope's name and id.
@@ -18,7 +18,7 @@ fun Scope.getSessionScope(): Scope {
 //region internal helper extensions
 @Synchronized
 internal fun Koin.getSessionScope(): Scope {
-    return getOrCreateScope(SESSION_SCOPE_ID, named<SessionManager>())
+    return getOrCreateScope(SESSION_SCOPE_ID, named(SESSION_SCOPE_NAME))
 }
 
 @Synchronized
